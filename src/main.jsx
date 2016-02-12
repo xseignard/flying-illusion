ff033452936f89525c57e30ae6e2dc80edd0170e
xseignard <xavier.seignard@gmail.com>
2016-02-12 15:24:28 +0100

    feat(fps counter with stats.js): Add a fps counter with stats.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import Stats from 'stats.js';
 import C from './constants';
 import './global';
 import { configureStore } from './store';
@@ -23,6 +24,18 @@ renderApp();
 if (module.hot) {
 	module.hot.accept();
 }
+
+// adds a stats.js fps counter
+const stats = new Stats();
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+document.body.appendChild(stats.domElement);
+const update = () => {
+	stats.update();
+	requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
 
 const scaleBody = () => {
 	const ratio = Math.min(
