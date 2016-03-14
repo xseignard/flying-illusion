a4c8f186a717ab3d16bee00459ebebcfd61c2575
xseignard <xavier.seignard@gmail.com>
2016-03-14 17:56:21 +0100

    fix(Zoom): Remove logo

--- src/components/Html/Zoom/index.jsx
+++ src/components/Html/Zoom/index.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import classnames from 'classnames';
 import Headline from '../common/Headline';
 import commonCss from '../common/css';
 import css from './css';
 
 export default () => {
-	const thisClass = classnames({
-		[commonCss.logo]: true,
-		[commonCss.out]: true
-	});
 	return (
 		<div className={css.zoom}>
-			<div className={thisClass}>
-				<img src="img/logo.png" />
-			</div>
 			<div className={commonCss.headline}>
 				<Headline direction="out" lineNumber="line1">
 					Prenez vos marques sur le tapis
